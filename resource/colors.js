e7236cbd6e2e87deab8057e1a98ec3a1d5b66e48
Tytianiuk Artem <tytyanyuk04@gmail.com>
2023-06-07 23:18:53 +0300

    Created 2 modules for colors and start chess position

---
+++ resource/colors.js
@@ -0,0 +1,4 @@
+export const COLORS = {
+  white: 'white',
+  black: 'black',
+}
---
+++ resource/position.js
@@ -0,0 +1,5 @@
+export const START_CHESS_POSITION = {
+  position: 'RNBQKBNR/PPPPPPPP/00000000/00000000/00000000/00000000/pppppppp/rnbqkbnr',
+  numberHorizontals: 8,
+  numberVerticals: 8,
+}
